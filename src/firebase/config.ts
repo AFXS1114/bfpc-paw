835c2174f0c179ff0286cc44ada84255b9abf22f
Alvin Brogada <alvinbrogada@gmail.com>
2026-04-14 02:02:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-95806379-26728",
+  "appId": "1:358708284671:web:95db2e617319c5319d10ca",
+  "apiKey": "AIzaSyBB8dGyuHWZcxQ19OjxOCN1qzYPqvb2IfQ",
+  "authDomain": "studio-95806379-26728.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "358708284671"
+};
